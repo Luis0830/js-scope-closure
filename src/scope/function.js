86224094dfbf46de3cb824bb313b6fed11599605
Luis Carlos Garcia <102985938+Luis0830@users.noreply.github.com>
2022-10-04 00:01:03 -0400

    scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+
+/// function scope
+function greeting() {
+    let useName = 'Ana';
+    console.log(useName);
+
+    if (useName === 'Ana'){
+        console.log(`Hi ${useName}`)
+    }
+}
+
+greeting();
---
+++ src/scope/global.js
@@ -0,0 +1,25 @@
+// Global Scope
+ var fruit = 'Apple'; //Global
+
+ function bestFruit(){
+    console.log(fruit);
+ }
+
+ bestFruit();
+
+////another example below//
+
+ function countries(){
+    country = 'Dominican Rep'; // global
+    console.log(country)
+ }
+
+  countries();
+  console.log(country);
+  ////////////
+
+
+
+
+
+
